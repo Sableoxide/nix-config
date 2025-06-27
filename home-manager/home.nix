616c0ae9a5fbad1c8aeaec5a619b0d508c47c7b3
sableoxide <gokofrank432@gmail.com>
2025-06-27 06:39:17 +0300

    home-manager back up

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -14,7 +14,7 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
-  home-manager.backupFileExtension = "backup";
+  programs.home-manager.backupFileExtension = "backup";
 
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
