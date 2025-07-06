92affa8a2b7318ca81d773031855eee2471d5b7a
sableoxide <gokofrank432@gmail.com>
2025-07-06 14:54:15 +0300

    removed home-manager use global packages option since it follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
         ./nixos/configuration.nix 
         home-manager.nixosModules.home-manager {
             home-manager = {
-              useGlobalPkgs = true;
               useUserPackages = true;
               backupFileExtension = ".backup";
               extraSpecialArgs = { inherit inputs username hostname; };
