7121e8a9cad305a93fbd5c53d4842d502f6f5758
sableoxide <gokofrank432@gmail.com>
2025-07-06 13:28:11 +0300

    set home-manager backup #try2

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,7 +15,7 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  home-manager.backupFileExtension = "backup";
+  backupFileExtension = "backup";
 
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
