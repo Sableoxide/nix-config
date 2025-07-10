ddde9ea4548f0aaaa05b35c30d42a136d7998bc6
sableoxide <gokofrank432@gmail.com>
2025-07-10 08:33:01 +0300

    new package python314

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,6 +140,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     rustup
+    pthon314
     wget
     kitty
     kbd
