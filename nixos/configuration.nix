6fd3ef67a95519c210556047ca1b64cb5b8ec72d
sableoxide <gokofrank432@gmail.com>
2025-07-10 09:04:54 +0300

    py 311 not 314

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,7 +140,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     rustup
-    python314
+    python311
     wget
     kitty
     kbd
