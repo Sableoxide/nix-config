df7af68289048607a031e18a67da5fad4e1cf7a5
sableoxide <gokofrank432@gmail.com>
2025-07-10 09:02:27 +0300

    rmv package steam

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,7 +140,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     rustup
-    steam
     python314
     wget
     kitty
