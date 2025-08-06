a90f30abf4013908dc0d55ac4a8bcd955148a277
sableoxide <gokofrank432@gmail.com>
2025-08-06 14:51:09 +0300

    update: home-manager

--- flake.nix
+++ flake.nix
@@ -21,9 +21,10 @@
       modules = [ 
         ./nixos/configuration.nix 
         home-manager.nixosModules.home-manager {
+            home-manager.backupFileExtension = "backup";
             home-manager = {
               useUserPackages = true;
-              backupFileExtension = "backup";
+              #backupFileExtension = "backup";
               extraSpecialArgs = { inherit inputs username hostname; };
               users.${username} = {
                 imports = [./home-manager/home.nix];
