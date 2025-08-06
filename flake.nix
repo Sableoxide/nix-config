71908ce0d8e26df4cc53572c87e7a610e55a3f5c
sableoxide <gokofrank432@gmail.com>
2025-08-06 14:59:55 +0300

    update: home-manager

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             #home-manager.backupFileExtension = "backup";
             home-manager = {
               useUserPackages = true;
-              backupFileExtension = "bakup";
+              backupFileExtension = "backup";
               extraSpecialArgs = { inherit inputs username hostname; };
               users.${username} = {
                 imports = [./home-manager/home.nix];
